7cf5ec08767e8296ff5a0061780dc79df53993bb
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-11-30 16:18:38 -0300

    ERA EL CONNECT EN EL CONSTRUCTOR LA REC*****************

--- common/hearbeat_listener.py
+++ common/hearbeat_listener.py
@@ -11,28 +11,30 @@ class HeartbeatListener(Process):
         self.quit = Value('i', 0, lock=True)
         self.dead_fun = dead_fun
 
+        self.host = host
         self.socket = None
-        self.connect(host)
 
     def connect(self, host):
         context = zmq.Context()
         self.socket = context.socket(zmq.PULL)
         self.socket.setsockopt(zmq.LINGER, -1)
-        self.socket.setsockopt(zmq.RCVTIMEO, 1000)
-        self.socket.connect(host)
+        self.socket.setsockopt(zmq.RCVTIMEO, 10000)
+        self.socket.connect("tcp://0.0.0.0:6002")
 
     def reconnect(self, host):
         self.socket.close()
         self.connect(host)
 
     def run(self):
+        self.connect(self.host)
+
         self.logger.info("Start receiving heartbeats")
         while not self.quit.value:
-            self.logger.info("Receiving")
-            beat = self.socket.recv()
-            if beat:
+            try:
+                self.logger.info("Receiving")
+                beat = self.socket.recv()
                 self.logger.info("Station heartbeat received: %r", beat)
-            else:
+            except zmq.error.Again:
                 self.logger.info("Station is dead. Finding new station's address and reconnecting")
                 host = self.dead_fun()
                 self.reconnect(host)
