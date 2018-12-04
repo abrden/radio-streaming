bc2240d6b9dc505301ad2fd3f9cb4caf477bbf1a
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-12-03 23:59:26 -0300

    Ring re-builts itself

--- station/ring_liveness_probe.py
+++ station/ring_liveness_probe.py
@@ -14,8 +14,11 @@ class NextAsker(Process):
         self.stations_total = stations_total
         self.stations_conns = {}
         self.answerer = PreviousAnswerer(self.station_num, self.stations_total, self.stations_conns)
+        self.poller = zmq.Poller()
+        self.others = []
+        self.next = self.next_station()
 
-    def connect_to_ring_pals(self): # FIXME
+    def connect_to_ring_pals(self):  # FIXME
         context = zmq.Context()
         if self.station_num == 1:
             conns = {}
@@ -51,6 +54,7 @@ class NextAsker(Process):
             socket.setsockopt(zmq.SNDTIMEO, 10000)
             socket.setsockopt(zmq.LINGER, -1)
             socket.connect("tcp://station3:6003")
+            socket.send_string("I'm your new next")
             self.stations_conns[3] = socket
 
         elif self.station_num == 2:
@@ -87,6 +91,7 @@ class NextAsker(Process):
             socket.setsockopt(zmq.SNDTIMEO, 10000)
             socket.setsockopt(zmq.LINGER, -1)
             socket.connect("tcp://station1:6003")
+            socket.send_string("I'm your new next")
             self.stations_conns[1] = socket
 
         elif self.station_num == 3:
@@ -123,6 +128,7 @@ class NextAsker(Process):
             socket.setsockopt(zmq.SNDTIMEO, 10000)
             socket.setsockopt(zmq.LINGER, -1)
             socket.connect("tcp://station2:6003")
+            socket.send_string("I'm your new next")
             self.stations_conns[2] = socket
         self.logger.info("Stations connections %r", self.stations_conns)
 
@@ -141,37 +147,49 @@ class NextAsker(Process):
         else:
             return self.stations_total
 
-    def send_live_probe(self):
-        next = self.next_station()
-        self.logger.info("Sending live probe msg to station %d", next)
-        socket = self.stations_conns[self.station_num][next]
+    def send_live_probe(self):  # TODO Ver que respete maquina de estados (recibir im alive del next y solo cambiar receptor si llega un Im your next)
+        self.logger.info("Sending 'Alive?' msg to station %d", self.next)
+        socket = self.stations_conns[self.station_num][self.next]
         try:
             socket.send_string("Alive?")
-            self.logger.info("Waiting for live probe response from station %d", next)
-            msg = socket.recv()
-            if msg.decode() == "I'm alive":
-                self.logger.info("Response received")
-                return True
         except zmq.error.Again:
-            self.logger.info("Timeout. Response not received")
+            self.logger.info("Timeout on send.")
             return False
+        self.logger.info("Waiting for response from station %d", self.next)
+        ok = False  # Will stay False in case of timeout
+        events = dict(self.poller.poll(100000))
+        for other in self.others:
+            if self.stations_conns[self.station_num][other] in events:
+                socket = self.stations_conns[self.station_num][other]
+                msg = socket.recv()
+                self.logger.info("Received msg from %d: %r", other, msg)
+                ok = True
+                if msg.decode() == "I'm alive":
+                    self.logger.info("I'm alive received")
+                if msg.decode() == "I'm your new next":
+                    self.logger.info("New next found: %d. Updating next variable", other)
+                    self.next = other
+        return ok
 
     def bypass_next_station(self):
         self.logger.info("Bypassing next node")
-        next = self.next_station()
+        next = self.next
         nextnext = self.next_station_to(next)
         self.logger.info("Nextnext is %d", nextnext)
         if nextnext == self.station_num:
             self.logger.info("Nextnext is me")
-            return  # FIXME wat do i do here
+            return  # TODO Implement
         socket = self.stations_conns[self.station_num][nextnext]
         socket.send_string("I'm your new previous")
-        self.logger.info("Overwriting next and prev stations")
-        self.stations_conns[self.station_num][next] = socket
+        self.logger.info("Updating next station variable to %d", nextnext)
+        self.next = nextnext
         self.logger.info("Stations connections %r", self.stations_conns)
 
     def run(self):
         self.connect_to_ring_pals()
+        self.others = self.stations_conns[self.station_num].keys()
+        for other in self.others:
+            self.poller.register(self.stations_conns[self.station_num][other], zmq.POLLIN)
         self.answerer.start()
 
         while True: # TODO graceful quit
@@ -199,11 +217,8 @@ class PreviousAnswerer(Thread):
         else:
             return self.stations_total
 
-    def answer_live_probe(self):
-        prev = self.previous_station()
-        self.logger.info("Waiting msg from station %d on socket %r", prev, self.stations_conns[prev])
-
-        events = dict(self.poller.poll(10000))
+    def answer_live_probe(self):  # TODO Ver que respete maquina de estados
+        events = dict(self.poller.poll(100000))
         for other in self.others:
             if self.stations_conns[other] in events:
                 socket = self.stations_conns[other]
@@ -213,10 +228,10 @@ class PreviousAnswerer(Thread):
                     if msg.decode() == "Alive?":
                         self.logger.info("Answering I'm alive")
                         socket.send_string("I'm alive")
-                    if msg.decode == "I'm your new previous":
+                    if msg.decode() == "I'm your new previous":
                         self.logger.info("I have a new previous")
                 except zmq.error.Again:
-                    self.logger.info("Timed out waiting for msg from %d to answer.", prev)
+                    self.logger.info("Timed out waiting for msg from previous to answer.")
                     continue  # Timeout to continue with the loop
 
     def run(self):
