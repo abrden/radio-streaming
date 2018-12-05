5c655f39296f141f37c63cea62b6800ebf031dad
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-12-04 21:58:20 -0300

    Implement AskIfLeader on transmitter

--- transmitter/middleware.py
+++ transmitter/middleware.py
@@ -1,27 +1,33 @@
-import time
 import logging
 
 import zmq
 
 from common.hearbeat_listener import HeartbeatListener
+from common.ask_if_leader import AskIfLeader
 
 
 class TransmitterMiddleware:
-    def __init__(self, country, freq):
+    def __init__(self, country, freq, stations_total):
         self.logger = logging.getLogger("TransmitterMiddleware")
+
+        self.stations_total = stations_total
+
+        self.logger.info("Searching for leader")
+        leader = AskIfLeader(stations_total).find_leader()
+        self.logger.info("Leader is %d", leader)
+        leader_addr = "tcp://station" + str(leader)
+
         self.country = country
         self.freq = freq
         
         context = zmq.Context()
         self.socket = context.socket(zmq.PUB)
         self.socket.setsockopt(zmq.LINGER, -1)
-        self.socket.connect("tcp://0.0.0.0:6000")
+        self.socket.connect(leader_addr + ":6000")
 
         self.logger.info("Starting StationHeartbeat")
-        self.heartbeart_monitor = HeartbeatListener("tcp://0.0.0.0:6002", self.new_leaders_addr)
+        self.heartbeart_monitor = HeartbeatListener(leader_addr + ":6002", self.new_leaders_addr)
         self.heartbeart_monitor.start()
-
-        time.sleep(2)
 
     def send(self, audio_chunk):
         topic = self.country + self.freq
@@ -36,5 +42,8 @@ class TransmitterMiddleware:
 
     def new_leaders_addr(self):
         self.logger.info("Findind new leader's address")
-        # TODO Find new leader
-        return "tcp://0.0.0.0:6002"
+        leader = AskIfLeader(self.stations_total).find_leader()
+        self.logger.info("Leader is %d", leader)
+        leader_addr = "tcp://station" + leader
+        # FIXME PUB on new leader
+        return leader_addr + ":6002"
